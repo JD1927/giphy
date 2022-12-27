ea57bbb0641be7e44c01242d7ca2ce28f997a0ae
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-26 22:10:03 -0500

    Add the GifComponent to use the found gif

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
+import Gif from '../../components/Gif';
 import GifsContext from '../../context/GifsContext';
 
 const Detail = ({ params }) => {
-
   const { id } = params;
   const { gifs } = useContext(GifsContext);
-  console.log({gifs});
 
-  return <div>{id}</div>;
+  const gif = gifs.find(singleGif => singleGif.id === id);
+
+  return <Gif {...gif}/>;
 };
 
 export default Detail;
