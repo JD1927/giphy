84bae1d7ff29181f4b807fe5d087edff490a9040
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-27 20:20:08 -0500

    Implements TrendingSearches on Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,8 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'wouter';
+import { useLocation } from 'wouter';
 import GifList from '../../components/GifList';
+import TrendingSearches from '../../components/TrendingSearches';
 import useGifs from '../../hooks/useGifs';
-
-const POPULAR_GIFS = [
-  'Shingeki No Kiojin Gifs',
-  'The Blacklist Gifs',
-  'Suits Gifs',
-  'Rick and Morty',
-];
 
 const Home = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -35,15 +29,8 @@ const Home = () => {
         />
       </form>
       <h3>Last search</h3>
-      <GifList gifs={gifs}/>
-      <h3>My favorites</h3>
-      <ul>
-        {POPULAR_GIFS.map((popularGif) => (
-          <li key={popularGif}>
-            <Link to={`/search/${popularGif}`}>{popularGif}</Link>
-          </li>
-        ))}
-      </ul>
+      <GifList gifs={gifs} />
+      <TrendingSearches/>
     </>
   );
 };
