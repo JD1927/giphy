a7b16ced60afbf11d51e79eee918c9576380d3c0
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-28 21:14:17 -0500

    Update useGifs hook to handle pagination

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,23 +2,39 @@ import { useContext, useEffect, useState } from 'react';
 import { getGifs } from '../services/getGifs';
 import GifsContext from '../context/GifsContext';
 
+const INITIAL_PAGE = 0;
+
 const useGifs = ({ keyword } = { keyword: null }) => {
   const [loading, setLoading] = useState(false);
+  const [loadingNextPage, setLoadingNextPage] = useState(false);
+  const [page, setPage] = useState(INITIAL_PAGE);
   const { gifs, setGifs } = useContext(GifsContext);
+
+  const keywordToUse = keyword || localStorage.getItem('lastKeyword');
 
   useEffect(() => {
     const fetchGifs = async () => {
       setLoading(true);
-      const keywordToUse = keyword || localStorage.getItem('lastKeyword');
       const requestedGifs = await getGifs({ keyword: keywordToUse });
       setGifs([...requestedGifs]);
       setLoading(false);
       localStorage.setItem('lastKeyword', keyword);
     }
     fetchGifs();
-  }, [keyword, setGifs]);
+  }, [keywordToUse, setGifs, keyword]);
 
-  return { loading, gifs };
+  useEffect(() => {
+    const handlePagination = async () => {
+      if (page === INITIAL_PAGE) return;
+      setLoadingNextPage(true);
+      const nextGifs = await getGifs({ keyword: keywordToUse, page });
+      setGifs(prevGifs => prevGifs.concat(nextGifs))
+      setLoadingNextPage(false);
+    };
+    handlePagination();
+  }, [page, keywordToUse, setGifs])
+
+  return { loading, gifs, loadingNextPage, setPage };
 }
 
 export default useGifs
