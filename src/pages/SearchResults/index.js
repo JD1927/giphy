36b5215f5c36cbefb2388af28c8eb94564fbf5d8
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-29 13:56:05 -0500

    Implement infinite scroll in SearchResults page

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import GifList from 'components/GifList';
 import useGifs from 'hooks/useGifs';
 import Spinner from 'components/Spinner';
 import './SearchResults.css';
+import useNearScreen from 'hooks/useNearScreen';
+import debounce from 'just-debounce-it';
 
 const SearchResults = ({ params }) => {
   const { keyword } = params;
   const { loading, gifs, setPage } = useGifs({ keyword });
 
-  const handleNextPage = () => setPage(prev => prev + 1);
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  });
+
+  const debounceHandleNextPage = useCallback(debounce(
+    () => setPage(prevPage => prevPage + 1), 200
+  ), [setPage]);
+
+  useEffect(() => {
+    if (isNearScreen) debounceHandleNextPage();
+  }, [isNearScreen, debounceHandleNextPage]);
 
   return (
-    <div className='SearchResults'>
-      {
-        loading
-          ? <Spinner/>
-          : (
-            <>
-              <h3>Results for: {decodeURI(keyword)}</h3>
-              <GifList gifs={gifs}/>
-              <button className='SearchResults-button' onClick={handleNextPage}>Get next page</button>
-            </>
-          )
-      }
+    <div className="SearchResults">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h3>Results for: {decodeURI(keyword)}</h3>
+          <GifList gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
+        </>
+      )}
     </div>
   );
 };
