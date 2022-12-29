21c593ca05e57ac3ff26e850ea28b520275ed417
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-28 21:14:34 -0500

    Refactors SearchResults page to handle pagination

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import GifList from 'components/GifList';
 import useGifs from 'hooks/useGifs';
+import Spinner from 'components/Spinner';
 
 const SearchResults = ({ params }) => {
   const { keyword } = params;
-  const { loading, gifs } = useGifs({ keyword });
+  const { loading, gifs, setPage } = useGifs({ keyword });
 
-  if (loading) return <span>Loading... 🌀</span>;
+  const handleNextPage = () => setPage(prev => prev + 1);
 
   return (
     <>
-      <h3>Results for: {decodeURI(keyword)}</h3>
-      <GifList gifs={gifs}/>
+      {
+        loading
+          ? <Spinner/>
+          : (
+            <>
+              <h3>Results for: {decodeURI(keyword)}</h3>
+              <GifList gifs={gifs}/>
+              <button onClick={handleNextPage}>Get next page</button>
+            </>
+          )
+      }
     </>
   );
 };
