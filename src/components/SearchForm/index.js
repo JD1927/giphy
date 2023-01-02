db0086a4fb4f777f733d18281e9175b4a7c58071
Juan Aguirre <j1927aguirre@gmail.com>
2023-01-01 20:20:46 -0500

    Extract search form from HomePage

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchForm = ({ onSubmit }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Go to another route
+    onSubmit({searchValue});
+  };
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Let's have fun..."
+        onChange={handleChange}
+        type="text"
+        value={searchValue}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'wouter';
 import GifList from 'components/GifList';
 import TrendingSearches from 'components/TrendingSearches';
 import useGifs from 'hooks/useGifs';
 import './Home.css';
+import SearchForm from 'components/SearchForm';
 
 const Home = () => {
-  const [searchValue, setSearchValue] = useState('');
   const [, goToRoute] = useLocation();
   const { gifs, loading } = useGifs();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = ({ searchValue }) => {
     // Go to another route
     goToRoute(`/search/${searchValue}`);
-  };
-  const handleChange = (event) => {
-    setSearchValue(event.target.value);
   };
   return (
     <div className='Home'>
       <h1 className='Home-title'>Most popular gifs</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Let's have fun..."
-          onChange={handleChange}
-          type="text"
-          value={searchValue}
-        />
-        <button type='submit'>Search</button>
-      </form>
+      <SearchForm onSubmit={handleSubmit}/>
       <h3>Last search</h3>
       <GifList gifs={gifs} loading={loading} />
       <TrendingSearches/>
